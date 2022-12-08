30d3f867ff7b56a6aeddfe9f34f98557efa4ccae
kamilWnek <kamil.wnek97@gmail.com>
2022-12-08 19:22:23 +0100

    add get task endpoint

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@tsed/di";
-import { BodyParams } from "@tsed/platform-params";
-import { Post } from "@tsed/schema";
+import { BodyParams, PathParams } from "@tsed/platform-params";
+import { Get, Post } from "@tsed/schema";
 import { TaskFacade } from "../facades/TaskFacade";
 import { TaskDto } from "../models/dto/TaskDto";
 import { Task } from "../models/entity/Task";
@@ -8,6 +8,12 @@ import { Task } from "../models/entity/Task";
 @Controller("/task")
 export class TaskController {
   constructor(private taskFacade: TaskFacade) {}
+
+  @Get("/:id")
+  async getTask(@PathParams("id") id: string): Promise<Task> {
+    const task = await this.taskFacade.getTask(id);
+    return { ...task };
+  }
 
   @Post("/")
   createTask(@BodyParams() taskDto: TaskDto): Promise<Task> {
